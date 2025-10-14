4a6d154f705c5f35ff52c302b4598533cd6912d7
rubyocatesc <19074179@saultcollege.ca>
2025-10-14 18:51:40 +0000

    M

--- src/main/java/Lab3/Player.java
+++
@@ -1,6 +0,0 @@
-package lab3;
-
-//Two possible players, Player X and Player O
-public enum Player {
-    X, O
-}
---
+++ src/main/java/Lab3/game/Board.java
@@ -0,0 +1,6 @@
+package lab3.game;
+
+public class Board {
+    // TODO: encapsulate the representation of the tictactoe board and provide instance methods to access and update it
+    
+}
